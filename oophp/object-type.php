8d3759e8d0277282efb10d1dd989374ca5917571
Rahmat <rahmat@example.com>
2022-04-17 16:35:49 +0700

    Object Type

--- oophp/object-type.php
+++ oophp/object-type.php
@@ -22,8 +22,17 @@ class Produk {
 
 }
 
+
+class CetakInfoProduk {
+  public function cetak( Produk $produk) {
+    $str = "{$produk->judul} | {$produk->getlabel()} (Rp. {$produk->harga})";
+    return $str;
+  }
+}
+
 // object
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 3000);
 
-echo $produk1->getlabel();
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
 
