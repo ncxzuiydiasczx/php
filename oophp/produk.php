2cd50c290c820d14f12648648d7ef62e68c69fc4
Rahmat <rahmat@example.com>
2022-04-17 16:20:24 +0700

    Property dan Method

--- oophp/produk.php
+++ oophp/produk.php
@@ -2,15 +2,27 @@
 
 // class
 class Produk {
+    
+    // property
     public  $judul, 
             $penulis,
             $penerbit,
             $harga;
 
+    // method
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+
 }
 
 // object
 $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// $produk1->penulis = "Masashi Kishimoto";
+// $produk1->penerbit = "Shonen Jump";
+// $produk1->harga = 3000;
 
-var_dump($produk1);
+echo $produk1->getlabel();
 
