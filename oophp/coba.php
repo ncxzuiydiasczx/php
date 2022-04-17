082b615b0d42fdb394f089c09a319b682ee29b72
Rahmat <rahmat@example.com>
2022-04-17 16:12:54 +0700

    Class dan Object

---
+++ oophp/coba.php
@@ -0,0 +1,12 @@
+<?php
+
+// class
+class Coba {
+
+
+
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
---
+++ oophp/produk.php
@@ -0,0 +1,16 @@
+<?php
+
+// class
+class Produk {
+    public  $judul, 
+            $penulis,
+            $penerbit,
+            $harga;
+
+}
+
+// object
+$produk1 = new Produk();
+
+var_dump($produk1);
+
