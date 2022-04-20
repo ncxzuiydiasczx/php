85d4b26bcd647592d9baa985866e926681e5e3a5
Rahmat <rahmat@example.com>
2022-04-20 10:49:17 +0700

    Constant

---
+++ oophp/constant.php
@@ -0,0 +1,16 @@
+<?php
+
+// define('NAMA', 'Rahmat Sumantri');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 27;
+// echo UMUR;
+
+// class Coba {
+//   const NAMA = 'Rahmat';
+// }
+
+// echo Coba::NAMA;
+
